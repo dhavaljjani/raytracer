a67a6ad662c65f7a4c3fa98b3c226363b1c38249
David Cook <d2cook@ucsd.edu>
2023-03-21 21:58:44 -0700

    Added readfile.h changes

--- readfile.h
+++ readfile.h
@@ -7,38 +7,38 @@
 using namespace std;
 
 class Sphere {
-    public:
-        vec3 sphere_center;
-        float r;
-        float color_ambient[3] = {0, 0, 0};
-        Sphere(vec3 center_input, float r);
+public:
+    vec3 sphere_center;
+    float r;
+    float color_ambient[3] = { 0, 0, 0 };
+    Sphere(vec3 center_input, float r);
 
     float findIntersection(vec3 p0, vec3 p1);
     void setColorAmbient(float a, float b, float c);
 };
 
 class Triangle {
-    public:
-        vec3 A;
-        vec3 B;
-        vec3 C;
-        float color_ambient[3] = { 0, 0, 0 };
-        Triangle(vec3 A, vec3 B, vec3 C);
+public:
+    vec3 A;
+    vec3 B;
+    vec3 C;
+    float color_ambient[3] = { 0, 0, 0 };
+    Triangle(vec3 A, vec3 B, vec3 C);
 
     float findIntersection(vec3 p0, vec3 p1);
     void setColorAmbient(float a, float b, float c);
 };
 
 class Light {
+
+public:
     vec3 light_posn;
     vec3 color;
     bool isPoint;
-
-    public:
-        Light(vec3 posn, vec3 color, bool isPoint);
+    Light(vec3 posn, vec3 color, bool isPoint);
 };
 
-void matransform (stack<mat4> &transfstack, int values[]);
-void rightmultiply (const mat4 & M, stack<mat4> &transfstack) ;
-bool readvals (stringstream &s, const int numvals, float values[]);
-void readfile (const char * filename) ;
+void matransform(stack<mat4>& transfstack, int values[]);
+void rightmultiply(const mat4& M, stack<mat4>& transfstack);
+bool readvals(stringstream& s, const int numvals, float values[]);
+void readfile(const char* filename);
